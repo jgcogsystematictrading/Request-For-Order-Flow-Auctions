19f8d6c5c0cb09cd295a52151473f06982399f18
doge_bull <69164627+jgcogsystematictrading@users.noreply.github.com>
2022-09-10 14:56:01 -0700

    Delete oev-relay directory

--- oev-relay/relay/check_winners.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-for api_key in ['jacob', 'midhav', 'burak']:
-    response = requests.get("http://192.168.1.93:33666/claim?key="+api_key)
-    print(f'{api_key}: {response.text}')
